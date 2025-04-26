58b2fa4b674b70054dc948d3e34829484fada26d
KoyoMiyazaki <70s.punkrock.development@gmail.com>
2025-04-26 21:31:16 +0900

    shadcn/uiをインストール

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,9 @@
+import { Button } from "@/components/ui/button";
+
 export default function Home() {
-  return <div className="container">Home Page</div>;
+  return (
+    <div className="container mx-auto px-[2rem]">
+      <Button>Test</Button>
+    </div>
+  );
 }
