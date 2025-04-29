e6d936215b83ef8dbe4db7d587dbcceee62075e7
KoyoMiyazaki <70s.punkrock.development@gmail.com>
2025-04-29 16:04:40 +0900

    投稿詳細API追加

---
+++ app/api/posts/[postId]/route.ts
@@ -0,0 +1,28 @@
+import prisma from "@/lib/prisma";
+import { NextResponse } from "next/server";
+
+export async function GET(
+  req: Request,
+  context: { params: { postId: string } }
+) {
+  const { postId } = context.params;
+
+  try {
+    const post = await prisma.post.findUnique({
+      where: { id: postId },
+    });
+    console.log(`postId: ${post?.id}`);
+
+    if (!post) {
+      return NextResponse.json(
+        { message: "投稿が見つかりません" },
+        { status: 404 }
+      );
+    }
+
+    return NextResponse.json(post);
+  } catch (error) {
+    console.error("投稿取得エラー:", error);
+    return NextResponse.json({ message: "サーバーエラー" }, { status: 500 });
+  }
+}
