1d71410bafb93ab200162f9285f5c015579b5545
KoyoMiyazaki <70s.punkrock.development@gmail.com>
2025-05-01 21:52:00 +0900

    投稿作成API追加

--- app/api/posts/route.ts
+++ app/api/posts/route.ts
@@ -1,4 +1,5 @@
 import prisma from "@/lib/prisma";
+import { NextResponse } from "next/server";
 
 export async function GET() {
   const posts = await prisma.post.findMany({
@@ -6,4 +7,22 @@ export async function GET() {
   });
 
   return Response.json(posts);
+}
+
+export async function POST(req: Request) {
+  const body = await req.json();
+  const { username, content } = body;
+
+  if (!username || !content) {
+    return NextResponse.json({ message: "不正な入力です" }, { status: 400 });
+  }
+
+  const post = await prisma.post.create({
+    data: {
+      username,
+      content,
+    },
+  });
+
+  return NextResponse.json(post, { status: 201 });
 }
