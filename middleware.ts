c86b7f95dbe83912657cf75f0e48949d25b48921
KoyoMiyazaki <70s.punkrock.development@gmail.com>
2025-05-03 22:03:33 +0900

    ログインしていない場合に/loginにリダイレクトするようにした

--- middleware.ts
+++ middleware.ts
@@ -1,10 +1,43 @@
-import { type NextRequest } from "next/server";
-import { updateSession } from "@/utils/supabase/middleware";
+import { createServerClient } from "@supabase/ssr";
+import { NextResponse } from "next/server";
+import type { NextRequest } from "next/server";
 
-export async function middleware(request: NextRequest) {
-  return await updateSession(request);
+export async function middleware(req: NextRequest) {
+  const res = NextResponse.next();
+
+  const supabase = createServerClient(
+    process.env.NEXT_PUBLIC_SUPABASE_URL!,
+    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
+    {
+      cookies: {
+        get(name) {
+          return req.cookies.get(name)?.value;
+        },
+        set(name, value, options) {
+          res.cookies.set(name, value, options);
+        },
+        remove(name, options) {
+          res.cookies.set(name, "", { ...options, maxAge: 0 });
+        },
+      },
+    }
+  );
+
+  const {
+    data: { session },
+  } = await supabase.auth.getSession();
+
+  console.log("middleware session:", session);
+
+  if (req.nextUrl.pathname.startsWith("/feed") && !session) {
+    const loginUrl = req.nextUrl.clone();
+    loginUrl.pathname = "/login";
+    return NextResponse.redirect(loginUrl);
+  }
+
+  return res;
 }
 
 export const config = {
-  matcher: ["/feed/:path"],
+  matcher: ["/feed/:path*"],
 };
