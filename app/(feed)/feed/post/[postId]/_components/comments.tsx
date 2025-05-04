5965ab1223512984a894428a9daa79b995940553
KoyoMiyazaki <70s.punkrock.development@gmail.com>
2025-05-04 21:57:48 +0900

    コメント一覧を表示できるようにした

--- app/(feed)/feed/post/[postId]/_components/comments.tsx
+++ app/(feed)/feed/post/[postId]/_components/comments.tsx
@@ -6,12 +6,23 @@ import { Label } from "@/components/ui/label";
 import { Textarea } from "@/components/ui/textarea";
 import { errorStyle, successStyle } from "@/lib/toast-style";
 import { cn } from "@/lib/utils";
-import React, { useState } from "react";
+import { Comment } from "@/types/comment";
+import React, { useEffect, useState } from "react";
 import { toast } from "sonner";
 
 export default function Comments({ postId }: { postId: string }) {
   const [username, setUsername] = useState<string>("");
   const [content, setContent] = useState<string>("");
+  const [comments, setComments] = useState<Comment[]>([]);
+
+  useEffect(() => {
+    const fetchComments = async () => {
+      const res = await fetch(`/api/comments?postId=${postId}`);
+      const data = await res.json();
+      setComments(data);
+    };
+    fetchComments();
+  }, [postId]);
 
   const handleSubmit = async (e: React.FormEvent) => {
     e.preventDefault();
@@ -61,6 +72,18 @@ export default function Comments({ postId }: { postId: string }) {
           コメントする
         </Button>
       </form>
+      <h3 className="text-lg font-bold">コメント一覧</h3>
+      <div className="space-y-4">
+        {comments.map((comment) => (
+          <div className="space-y-2 border p-4 rounded-md">
+            <p className="font-bold">{comment.username}</p>
+            <p>{comment.content}</p>
+            <p className="text-sm text-muted-foreground">
+              {new Date(comment.createdAt).toLocaleString()}
+            </p>
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
