88ceb35492db30cb00eedf55d635ada2a1817dbd
KoyoMiyazaki <70s.punkrock.development@gmail.com>
2025-05-17 18:41:49 +0900

    タグのスタイルを修正

--- components/tag.tsx
+++ components/tag.tsx
@@ -8,7 +8,7 @@ export default function Tag({ name }: TagProps) {
   return (
     <div className="flex gap-1 items-center border border-muted-foreground rounded-sm p-1 text-accent-foreground">
       <TagIcon className="w-4 h-4" />
-      <span className="text-sm">{name}</span>
+      <span className="text-xs">{name}</span>
     </div>
   );
 }
