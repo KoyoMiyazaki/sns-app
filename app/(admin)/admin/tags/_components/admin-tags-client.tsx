27d6b081e836c199132bf039829736418d268d2d
KoyoMiyazaki <70s.punkrock.development@gmail.com>
2025-06-19 22:18:02 +0900

    読み込み時にスケルトン、タグがない場合にEmptyの表示をするようにした

--- app/(admin)/admin/tags/_components/admin-tags-client.tsx
+++ app/(admin)/admin/tags/_components/admin-tags-client.tsx
@@ -3,15 +3,24 @@
 import { Tag as TagType } from "@/types/tag";
 import { useEffect, useState } from "react";
 import DeleteTagButton from "./delete-tag-button";
+import OneLineSkeleton from "@/components/one-line-skeleton";
+import EmptyComponent from "@/components/empty-component";
 
 export default function AdminTagsClient() {
   const [tags, setTags] = useState<TagType[]>([]);
+  const [loading, setLoading] = useState<boolean>(true);
 
   useEffect(() => {
     const fetchTags = async () => {
-      const res = await fetch("/api/tags");
-      const data = await res.json();
-      setTags(data);
+      try {
+        const res = await fetch("/api/tags");
+        const data = await res.json();
+        setTags(data);
+      } catch (error) {
+        console.error("タグ一覧取得エラー", error);
+      } finally {
+        setLoading(false);
+      }
     };
 
     fetchTags();
@@ -20,17 +29,27 @@ export default function AdminTagsClient() {
   return (
     <div className="flex flex-col gap-8 w-[350px] md:w-[400px]">
       <h1 className="text-2xl font-bold">タグ管理</h1>
-      <div className="flex flex-col divide-y border-y">
-        {tags.map((tag) => (
-          <div
-            key={tag.id}
-            className="flex items-center justify-between px-2 py-4 hover:bg-blue-50"
-          >
-            <p className="font-bold">{tag.name}</p>
-            <DeleteTagButton tagId={tag.id} />
-          </div>
-        ))}
-      </div>
+      {loading ? (
+        <div className="divide-y border-y">
+          {Array.from({ length: 3 }).map((_, i) => (
+            <OneLineSkeleton key={i} />
+          ))}
+        </div>
+      ) : tags.length === 0 ? (
+        <EmptyComponent objectName={"タグ"} />
+      ) : (
+        <div className="flex flex-col divide-y border-y">
+          {tags.map((tag) => (
+            <div
+              key={tag.id}
+              className="flex items-center justify-between px-2 py-4 hover:bg-blue-50"
+            >
+              <p className="font-bold">{tag.name}</p>
+              <DeleteTagButton tagId={tag.id} />
+            </div>
+          ))}
+        </div>
+      )}
     </div>
   );
 }
